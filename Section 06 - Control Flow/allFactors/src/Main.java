835df9f07c4667a87b8cbff840f7896d1cb49605
bj-pieri <antoniompieri@gmail.com>
2022-12-01 22:25:15 -0300

    add new study exercise (allFactors)

---
+++ Section 06 - Control Flow/allFactors/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        printFactors(12);
+    }
+
+    public static void printFactors(int number) {
+        if (number < 0) {
+            System.out.println("Invalid Value");;
+        }
+
+        for(int looper = 1;looper <= number; looper++) {
+            int factorial = looper;
+            if (number % factorial == 0 ) System.out.println(factorial);
+        }
+    }
+}
